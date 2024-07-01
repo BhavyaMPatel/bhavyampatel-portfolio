fe6efe50cd5155746495a4d93cd3465b7689eb41
BhavyaMPatel <bhavyapatel4012@gmail.com>
2024-07-01 12:03:24 +0530

    navbar v1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Componets/Navbar";
+
+export default function App() {
+  return (
+    <div className="bg-gray-50">
+      <div className="md:ml-36 md:mr-36 bg-white h-screen">
+          <Navbar/>
+          
+          <div>
+
+          </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/Componets/Navbar.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+  return (
+    <>
+        <div className="flex justify-center items-center">
+            <div className="m-5 p-3 rounded-xl border-solid border-2 border-gray-50 shadow-md shadow-blue-50 font-thin">
+                <div className="flex justify-center space-x-6 cursor-pointer">
+                    <ul>Home</ul>
+                    <ul>About</ul>
+                    <ul>Project</ul>
+                    <ul>Skills</ul>
+                    <ul>Contact</ul>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
