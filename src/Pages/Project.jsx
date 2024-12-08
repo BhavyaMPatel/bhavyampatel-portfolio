79f6f94699b243190ffb0d8fc411a41dc6db3cc2
BhavyaMPatel <bhavyapatel4012@gmail.com>
2024-12-08 19:23:21 +0530

    seo update text parsing

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -46,59 +46,74 @@ export default function Project() {
         },
     ];
 
-    return (
-        <>
-              <Helmet>   
-                <title>BhavyaMPatel | Project Page</title>
-                <meta name="description" content="
-                Subtitle Generator • Effortless subtitling for global audiences,
-                Advertisement Recommendation • Captivate audience with compelling advertisement,
-                RetailRevolutioner • Insightful retail predictions,
-                SecureScanner • PII Masker,
-                authbapp • npm package for appwrite authentication solution,
-                Your-Own-Gdrive • a webapplication for end to end cloud storage
-                "/>
-                <link rel="canonical" href="https://bhavyapatel.vercel.app/projects" />
-            </Helmet>
+  return (
+    <>
+      <Helmet>
+        <title>BhavyaMPatel | Project Page</title>
+        <meta
+          name="description"
+          content="Subtitle Generator • Effortless subtitling for global audiences, Advertisement Recommendation • Captivate audience with compelling advertisement, RetailRevolutioner • Insightful retail predictions, SecureScanner • PII Masker, authbapp • npm package for appwrite authentication solution, Your-Own-Gdrive • a webapplication for end to end cloud storage"
+        />
+        <link rel="canonical" href="https://bhavyapatel.vercel.app/projects" />
+      </Helmet>
 
-            <div className="sm:px-8 mt-16 sm:mt-32">
-                <div className="mx-auto max-w-7xl lg:px-8">
-                    <div className="relative px-4 sm:px-8 lg:px-12">
-                        <div className="mx-auto max-w-2xl lg:max-w-5xl">
-                            <header className="max-w-2xl">
-                                <h1 className="text-4xl tracking-tight text-zinc-800 sm:text-5xl font-thin">Fuel of Software Engineer's <br /> ~ Projects</h1>
-                                <p className="mt-6 text-base text-black font-thin">These are my open-source projects which I created over a span of 3 years. Feel free to contribute! Happy developing 🧑‍💻.</p>
-                            </header>
+      <div className="sm:px-8 mt-16 sm:mt-32">
+        <div className="mx-auto max-w-7xl lg:px-8">
+          <div className="relative px-4 sm:px-8 lg:px-12">
+            <div className="mx-auto max-w-2xl lg:max-w-5xl">
+              <header className="max-w-2xl">
+                <h1 className="text-4xl tracking-tight text-zinc-800 sm:text-5xl font-thin">
+                  Fuel of Software Engineer's <br /> ~ Projects
+                </h1>
+                <p className="mt-6 text-base text-black font-thin">
+                  These are my open-source projects which I created over a span of 3 years. Feel free to contribute! Happy developing 🧑‍💻.
+                </p>
+              </header>
 
-                            <div className="mt-16 sm:mt-20">
-                                <ul role="list" className="grid grid-cols-1 gap-x-20 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
-                                    {Data.map(({ src, url, title, about, technology }) => (
-                                        <div key={src}>
-                                            <li className="group relative flex flex-col items-start">
-                                                <div className="relative z-10 flex h-28 w-full items-center justify-center rounded-xl bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 ">
-                                                    <img alt="" src={src} width="1696" height="455" decoding="async" data-nimg="1" className="w-full h-full object-cover rounded-xl" loading="lazy"/>
-                                                </div>
-                                                <h2 className="mt-6 text-base font-thin text-zinc-800">
-                                                    <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100  sm:-inset-x-6 sm:rounded-2xl"></div>
-                                                    <a href={url}><span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span className="relative z-10 font-thin">{title}</span></a>
-                                                </h2>
-                                                <p className="relative z-10 mt-2 text-sm font-thin text-black">{about}</p>
-                                                <p className="tag relative z-10 mt-2 text-sm text-black font-thin">{technology}</p>
-                                                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500">
-                                                    <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
-                                                    <path d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z" fill="currentColor"></path>
-                                                    </svg>
-                                                    <span className="ml-2"><a src={url}>github link</a></span>
-                                                </p>
-                                            </li>
-                                        </div>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="mt-16 sm:mt-20">
+                <ul role="list" className="grid grid-cols-1 gap-x-20 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
+                  {Data.map(({ src, url, title, about, technology }) => (
+                    <li key={src} className="group relative flex flex-col items-start">
+                      <div className="relative z-10 flex h-28 w-full items-center justify-center rounded-xl bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5">
+                        <img
+                          alt={title}
+                          src={src}
+                          width="1696"
+                          height="455"
+                          decoding="async"
+                          data-nimg="1"
+                          className="w-full h-full object-cover rounded-xl"
+                          loading="lazy"
+                        />
+                      </div>
+                      <h2 className="mt-6 text-base font-thin text-zinc-800">
+                        <a href={url} target="_blank" rel="noopener noreferrer">
+                          <span className="relative z-10 font-thin">{title}</span>
+                        </a>
+                      </h2>
+                      <p className="relative z-10 mt-2 text-sm font-thin text-black">{about}</p>
+                      <p className="tag relative z-10 mt-2 text-sm text-black font-thin">{technology}</p>
+                      <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500">
+                        <svg viewBox="0 0 24 24" aria-hidden="true" className="h-6 w-6 flex-none">
+                          <path
+                            d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Z..."
+                            fill="currentColor"
+                          />
+                        </svg>
+                        <span className="">
+                          <a href={url} target="_blank" rel="noopener noreferrer">
+                            github link
+                          </a>
+                        </span>
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
